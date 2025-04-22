dd9feea6ea799d6ccb5cddbc2a056c2e30110d71
caver-yan <990171527@qq.com>
2025-04-22 09:46:51 +0800

    feat: 冗余日志删除

--- src/views/classify/productList.vue
+++ src/views/classify/productList.vue
@@ -14,7 +14,7 @@
         </template>
       </el-table-column>
       <el-table-column prop="productName" label="产品名称"></el-table-column>
-      <el-table-column prop="statusName" label="状态"></el-table-column>
+      <!-- <el-table-column prop="statusName" label="状态"></el-table-column> -->
       <el-table-column label="操作">
         <template #default="scope">
           <el-button @click="showDetails(scope.row)">详情</el-button>
@@ -92,7 +92,6 @@ const debouncedFetch = debounce(() => {
 watch(
   () => props.taskStatus,
   newVal => {
-    console.log("taskStatus changed:", newVal);
     debouncedFetch();
   },
   { immediate: true }
