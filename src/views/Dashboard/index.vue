97e436c4156cc4329b821118deb5f28cb28efcff
caver-yan <990171527@qq.com>
2025-05-02 23:37:15 +0800

    feat: 展示调整

--- src/views/Dashboard/index.vue
+++ src/views/Dashboard/index.vue
@@ -6,6 +6,17 @@
         <h2 class="text-2xl font-bold">Dashboard</h2>
       </div>
     </div>
+    <el-descriptions title="User Info" column="3">
+      <el-descriptions-item label="Username">kooriookami</el-descriptions-item>
+      <el-descriptions-item label="Telephone">18100000000</el-descriptions-item>
+      <el-descriptions-item label="Place">Suzhou</el-descriptions-item>
+      <el-descriptions-item label="Remarks">
+        <el-tag size="small">School</el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item label="Address">
+        No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu Province
+      </el-descriptions-item>
+    </el-descriptions>
   </div>
 </template>
 
