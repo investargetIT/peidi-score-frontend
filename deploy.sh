aabf78b80cc6a01781095e66c371fee2ef7891bb
caver-yan <990171527@qq.com>
2025-05-08 23:30:16 +0800

    feat: 部署文件调整

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,15 @@
 pnpm run build
 
 # 重命名 dist 目录
-mv dist pms
+mv dist score
 
 # 获取当前路径
 CURRENT_PATH=$(pwd)
 
 # 上传到服务器
-scp -r $CURRENT_PATH/pms root@121.43.145.161:/media/www
+scp -r $CURRENT_PATH/score root@121.43.145.161:/media/www
 
 # 删除本地 pms 文件夹
-rm -rf pms
+rm -rf score
 
 echo "部署完成！"
