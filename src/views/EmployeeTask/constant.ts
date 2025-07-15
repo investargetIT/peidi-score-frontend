1ab7efc46de5ef377f39e72bf9d2240ffe9f149e
言午 <990171527@qq.com>
2025-07-15 15:25:22 +0800

    feat: 待办题库更新

--- src/views/EmployeeTask/constant.ts
+++ src/views/EmployeeTask/constant.ts
@@ -21,32 +21,68 @@ export const TaskLevelList = {
       key: "beginner-5"
     },
     {
-      text: "说出所在部门所有成员的名字及爱好",
+      text: "说出所在部门/小组所有成员的名字及爱好",
       key: "beginner-6"
     },
     {
-      text: "参加一次部门会议",
+      text: "使用钉钉完成一次会议预定",
       key: "beginner-7"
     },
     {
-      text: "使用钉钉完成一次会议预定",
+      text: "说出工位所在办公区的会议室名称",
       key: "beginner-8"
     },
     {
-      text: "一周不出现迟到&早退行为",
+      text: "一周保持工位干净整洁（下班拍照记录）",
       key: "beginner-9"
     },
     {
-      text: "拍摄一张和公司宠物的合影",
+      text: "拍摄一张和公司猫咪的合影，并说出它的名字",
       key: "beginner-10"
     },
     {
-      text: "说出对应办公区的会议室名称",
+      text: "拍摄一张和同事宠物的合影，并说出它的名字",
       key: "beginner-11"
     },
     {
-      text: "一周保持工位干净整洁",
+      text: "拍摄一个你最喜欢的公司角落",
       key: "beginner-12"
+    },
+    {
+      text: "请和Lucy总合影一张",
+      key: "beginner-13"
+    },
+    {
+      text: "请和庄总合影一张",
+      key: "beginner-14"
+    },
+    {
+      text: "请和庄总合影一张",
+      key: "beginner-14"
+    },
+    {
+      text: "请和陈董合影一张",
+      key: "beginner-15"
+    },
+    {
+      text: "请和游总合影一张",
+      key: "beginner-16"
+    },
+    {
+      text: "请和Nikki总合影一张",
+      key: "beginner-17"
+    },
+    {
+      text: "找一个和你同星座的同事，和她/他合影一张，并说出她/他的名字、部门、岗位",
+      key: "beginner-18"
+    },
+    {
+      text: "找一个和你同姓氏的同事，和她/他合影一张，并说出她/他的名字、部门、岗位",
+      key: "beginner-19"
+    },
+    {
+      text: "认识会议室，找到佩蒂所有会议室，并上传照片",
+      key: "beginner-20"
     }
   ],
   Intermediate: [
@@ -59,16 +95,28 @@ export const TaskLevelList = {
       key: "intermediate-2"
     },
     {
-      text: "主动协助同事完成1项非本职但你能胜任的小任务（如翻译文档/测试样品）",
+      text: '拍摄办公区最能体现"保鲜"文化的角落/场景，附50字说明',
       key: "intermediate-3"
     },
     {
-      text: "梳理本周工作中主要工作任务的标准化处理流程",
+      text: '拍摄办公区最能体现"担当"文化的角落/场景，附50字说明',
       key: "intermediate-4"
     },
     {
-      text: "预测你当前岗位3个月内最可能出现的2个风险点/困难",
+      text: '拍摄办公区最能体现"靠谱"文化的角落/场景，附50字说明',
       key: "intermediate-5"
+    },
+    {
+      text: "主动协助同事完成1项非本职但你能胜任的小任务（如翻译文档/测试样品）",
+      key: "intermediate-6"
+    },
+    {
+      text: "梳理本周工作中主要工作任务的标准化处理流程",
+      key: "intermediate-7"
+    },
+    {
+      text: "预测你当前岗位3个月内最可能出现的2个风险点/困难",
+      key: "intermediate-8"
     }
   ],
   Advanced: [
