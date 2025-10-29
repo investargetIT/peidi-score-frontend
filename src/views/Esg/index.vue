9c4fd5d4752131f2e089ec0e76ec4fa26d0442c1
Dawn <hyshen_olvae@zohomail.com>
2025-10-29 16:39:40 +0800

    feat: 优化ESG逻辑

--- src/views/Esg/index.vue
+++ src/views/Esg/index.vue
@@ -1,53 +1,63 @@
 <template>
-  <div class="esg-management">
-    <!-- 顶部导航标签 -->
-    <div class="flex items-center justify-between">
-      <div class="mr-[10px]">
-        <el-select
-          v-model="yearValue"
-          placeholder="请选择年度"
-          style="width: 200px"
-          size="large"
-        >
-          <template #label="{ label, value }">
-            <span class="text-[14px]">年度: </span>
-            <span class="text-[16px] font-bold">{{ value }}</span>
-          </template>
-          <el-option
-            v-for="item in yearOptions"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
-          />
-        </el-select>
-      </div>
-      <div class="flex-1">
-        <el-tabs v-model="activeTab" class="esg-tabs">
-          <el-tab-pane label="公司概况" name="company-overview" />
-          <el-tab-pane label="公司治理" name="corporate-governance" />
-          <el-tab-pane label="ESG管理" name="esg-management" />
-          <el-tab-pane label="产业发展与运营" name="business-operations" />
-          <el-tab-pane label="质量与食品安全管理" name="quality-food-safety" />
-          <el-tab-pane label="供应链管理" name="supplier-management" />
-          <el-tab-pane
-            label="信息安全与隐私保护"
-            name="information-security-privacy"
-          />
-          <el-tab-pane label="员工" name="employees" />
-          <el-tab-pane label="环境影响" name="environmental-impact" />
-          <el-tab-pane label="回馈社会" name="community-welfare" />
-        </el-tabs>
-      </div>
-    </div>
-
-    <!-- 动态组件内容区域 -->
-    <!-- :isEdit="hasEditPermission(activeTab)" -->
-    <component
-      :is="currentComponent"
-      :active-tab="activeTab"
-      :isEdit="hasEditPermission(activeTab)"
-      :year="yearValue"
+  <div class="w-[100%] h-[100%]">
+    <YearCard
+      :yearOptions="yearOptions"
+      :submit="handleYearSubmit"
+      v-show="showYearCard"
     />
+    <div class="esg-management" v-show="!showYearCard">
+      <!-- 顶部导航标签 -->
+      <div class="flex items-center justify-between">
+        <div class="mr-[10px]">
+          <el-select
+            v-model="yearValue"
+            placeholder="请选择年度"
+            style="width: 200px"
+            size="large"
+          >
+            <template #label="{ label, value }">
+              <span class="text-[14px]">年度: </span>
+              <span class="text-[16px] font-bold">{{ value }}</span>
+            </template>
+            <el-option
+              v-for="item in yearOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            />
+          </el-select>
+        </div>
+        <div class="flex-1">
+          <el-tabs v-model="activeTab" class="esg-tabs">
+            <el-tab-pane label="公司概况" name="company-overview" />
+            <el-tab-pane label="公司治理" name="corporate-governance" />
+            <el-tab-pane label="ESG管理" name="esg-management" />
+            <el-tab-pane label="产业发展与运营" name="business-operations" />
+            <el-tab-pane
+              label="质量与食品安全管理"
+              name="quality-food-safety"
+            />
+            <el-tab-pane label="供应链管理" name="supplier-management" />
+            <el-tab-pane
+              label="信息安全与隐私保护"
+              name="information-security-privacy"
+            />
+            <el-tab-pane label="员工" name="employees" />
+            <el-tab-pane label="环境影响" name="environmental-impact" />
+            <el-tab-pane label="回馈社会" name="community-welfare" />
+          </el-tabs>
+        </div>
+      </div>
+
+      <!-- 动态组件内容区域 -->
+      <!-- :isEdit="hasEditPermission(activeTab)" -->
+      <component
+        :is="currentComponent"
+        :active-tab="activeTab"
+        :isEdit="hasEditPermission(activeTab)"
+        :year="yearValue"
+      />
+    </div>
   </div>
 </template>
 
@@ -65,6 +75,7 @@ import SupplierManagement from "./components/SupplierManagement.vue";
 import CommunityWelfare from "./components/CommunityWelfare.vue";
 import ProductsServices from "./components/ProductsServices.vue";
 import { hasEditPermission } from "./utils";
+import YearCard from "./yearCard.vue";
 
 const { t } = useI18n();
 
@@ -119,6 +130,16 @@ for (let i = 0; i < 10; i++) {
     label: (2025 + i).toString()
   });
 }
+//#endregion
+
+//#region 首次选择年份
+// 首次遮罩层
+const showYearCard = ref(true);
+const handleYearSubmit = year => {
+  yearValue.value = year;
+  // 隐藏遮罩层
+  showYearCard.value = false;
+};
 //#endregion
 </script>
 
---
+++ src/views/Esg/yearCard.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+import RiCalendarLine from "@iconify-icons/ri/calendar-line";
+
+// props
+const props = defineProps({
+  yearOptions: {
+    type: Array<{ value: string; label: string }>,
+    required: true
+  },
+  submit: {
+    type: Function,
+    required: true
+  }
+});
+
+const form = reactive({
+  esgYear: new Date().getFullYear().toString()
+});
+
+const handleSubmit = () => {
+  props.submit(form.esgYear);
+  console.log(form.esgYear);
+};
+</script>
+
+<template>
+  <div class="w-[100%] h-[100%] flex items-center justify-center">
+    <el-card
+      style="width: 500px; border-radius: 15px"
+      class="flex justify-center"
+    >
+      <!-- 图标 -->
+      <div class="flex items-center justify-center">
+        <div
+          class="w-[64px] h-[64px] flex items-center justify-center bg-[#dbeafe] rounded-[64px]"
+        >
+          <IconifyIconOffline
+            :icon="RiCalendarLine"
+            class="text-[#155dfc] text-[32px]"
+          />
+        </div>
+      </div>
+      <!-- 标题 -->
+      <div class="text-[#0a0a0a] text-[24px] font-bold text-center mt-[24px]">
+        选择ESG报告年份
+      </div>
+      <div class="text-[#737373] text-[16px] text-center mt-[16px]">
+        请选择你要填写的ESG信息所属年份
+      </div>
+      <!-- 表单 -->
+      <el-form label-position="top" class="w-[450px] mt-[24px]">
+        <el-form-item label="报告年份" prop="esgYear" class="text-[#000]">
+          <el-select v-model="form.esgYear" placeholder="请选择报告年份">
+            <el-option
+              v-for="year in yearOptions"
+              :key="year.value"
+              :label="year.label + '年'"
+              :value="year.value"
+            />
+          </el-select>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button
+            color="#0a0a0a"
+            type="primary"
+            class="w-[100%]"
+            style="height: 40px; font-size: 14px; border-radius: 5px"
+            @click="handleSubmit"
+            >进入填写</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+:deep(.el-form-item__label) {
+  margin-bottom: 4px;
+  font-size: 14px;
+  color: #0a0a0a;
+}
+
+:deep(.el-select__wrapper) {
+  height: 36px;
+}
+
+:deep(.el-select__selected-item) {
+  font-size: 14px;
+  color: #0a0a0a;
+}
+</style>
